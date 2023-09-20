3b0b08568f8e2383a0704170447262c97b6f05b5
Tony Todorov <tonytodorov@protonmail.com>
2023-09-20 20:54:33 +0300

    Rename PB Exam class name

--- Java-Programming-Basics/Exams/Pre-Exam-2023-June-24/src/UniquePinCodes.java
+++ Java-Programming-Basics/Exams/Pre-Exam-2023-June-24/src/UniquePinCodes.java
@@ -1,6 +1,6 @@
 import java.util.Scanner;
 
-public class UniquePINCodes {
+public class UniquePinCodes {
 
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
