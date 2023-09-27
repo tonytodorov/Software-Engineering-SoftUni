eead5ec2b3882c41d7e592593c47f75d30be35d7
Tony Todorov <tonytodorov@protonmail.com>
2023-09-27 21:58:26 +0300

    Update _07_CondenseArrayToNumber.java

--- Java-Programming-Fundamentals/03-Arrays/Lecture/src/_07_CondenseArrayToNumber.java
+++ Java-Programming-Fundamentals/03-Arrays/Lecture/src/_07_CondenseArrayToNumber.java
@@ -7,34 +7,21 @@ public class _07_CondenseArrayToNumber {
         Scanner scanner = new Scanner(System.in);
 
         int[] numbers = Arrays.stream(scanner.nextLine()
-                .split(" "))
+                        .split(" "))
                 .mapToInt(Integer::parseInt)
                 .toArray();
 
-        if(numbers.length - 1 == 0){
-            System.out.println(numbers[0]);
-            return;
-        }
-
-        int i = 0;
-        int sum = 0;
+        int[] temp = new int[numbers.length - 1];
 
         while (numbers.length - 1 > 0){
-
-            int[] condensed = new int[numbers.length - 1];
-
-            while (i < numbers.length - 1){
-                condensed[i] = numbers[i] + numbers[i + 1];
-                i++;
+            for (int i = 0; i < temp.length; i++) {
+                temp[i] = numbers[i] + numbers[i + 1];
             }
 
-            System.arraycopy(condensed, 0, numbers, 0, numbers.length - 1);
-
-            i = 0;
-            sum = condensed[0];
-            numbers = condensed;
+            numbers = temp;
+            temp = new int[numbers.length - 1];
         }
 
-        System.out.println(sum);
+        System.out.println(numbers[0]);
     }
 }
