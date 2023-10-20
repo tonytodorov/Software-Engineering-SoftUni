4b5ffc5d1b9af0ebe75358202f6579f7a53cdcf6
Tony Todorov <tonytodorov@protonmail.com>
2023-10-20 16:05:43 +0300

    Update _03_ManOWar.java

--- Java-Programming-Fundamentals/06-Exam-Preparation/Exam-Unknown-Date-2/src/_03_ManOWar.java
+++ Java-Programming-Fundamentals/06-Exam-Preparation/Exam-Unknown-Date-2/src/_03_ManOWar.java
@@ -17,7 +17,7 @@ public class _03_ManOWar {
 
         addElements(secondShip, warship);
 
-        int health = Integer.parseInt(scanner.nextLine());
+        int maxHealth = Integer.parseInt(scanner.nextLine());
         String command = scanner.nextLine();
 
         while (!command.equals("Retire")) {
@@ -33,14 +33,66 @@ public class _03_ManOWar {
 
                 if (isIndexValid) {
                     warship.set(index, warship.get(index) - damage);
+
+                    boolean isSectionBreak = isSectionBreak(warship);
+
+                    if (isSectionBreak) {
+                        System.out.println("You won! The enemy ship has sunken.");
+                        return;
+                    }
+                }
+            } else if (operation.equals("Defend")) {
+                int startIndex = Integer.parseInt(tokens[1]);
+                int endIndex = Integer.parseInt(tokens[2]);
+                int damage = Integer.parseInt(tokens[3]);
+
+                boolean isStarIndexValid = isIndexValid(pirateShip, startIndex);
+                boolean isEndIndexValid = isIndexValid(pirateShip, endIndex);
+
+                if (isStarIndexValid && isEndIndexValid) {
+                    for (int i = startIndex; i <= endIndex; i++) {
+                        pirateShip.set(i, pirateShip.get(i) - damage);
+                    }
+
+                    boolean isSectionBreak = isSectionBreak(pirateShip);
+
+                    if (isSectionBreak) {
+                        System.out.println("You lost! The pirate ship has sunken.");
+                        return;
+                    }
+                }
+            } else if (operation.equals("Repair")) {
+                int index = Integer.parseInt(tokens[1]);
+                int health = Integer.parseInt(tokens[2]);
+
+                boolean isIndexValid = isIndexValid(pirateShip, index);
+
+                if (isIndexValid) {
+                    pirateShip.set(index, pirateShip.get(index) + health);
+
+                    if (pirateShip.get(index) > maxHealth) {
+                       pirateShip.set(index, maxHealth);
+                    }
+                }
+            } else if (operation.equals("Status")) {
+                double bound = maxHealth * 0.2;
+
+                int count = 0;
+
+                for (int i = 0; i < pirateShip.size(); i++) {
+                    if (pirateShip.get(i) < bound) {
+                        count += 1;
+                    }
                 }
 
+                System.out.printf("%d sections need repair.%n", count);
             }
-
-
 
             command = scanner.nextLine();
         }
+
+        System.out.printf("Pirate ship status: %d%n", sumSectionShip(pirateShip));
+        System.out.printf("Warship status: %d", sumSectionShip(warship));
     }
 
 
@@ -53,5 +105,27 @@ public class _03_ManOWar {
 
     public static boolean isIndexValid(List<Integer> list, int index) {
         return index >= 0 && index <= list.size();
+    }
+
+    public static boolean isSectionBreak(List<Integer> list) {
+
+        for (Integer integer : list) {
+            if (integer <= 0) {
+                return true;
+            }
+        }
+
+        return false;
+    }
+
+    public static int sumSectionShip(List<Integer> list) {
+
+        int sum = 0;
+
+        for (Integer element : list) {
+            sum += element;
+        }
+
+        return sum;
     }
 }
