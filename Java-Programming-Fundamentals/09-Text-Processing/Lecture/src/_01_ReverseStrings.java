5168f49319129547805ab9ef6459ad47877ab5f9
tonytodorov <tonytodorov@protonmail.com>
2023-11-04 18:42:43 +0200

    Update _01_ReverseStrings.java

--- Java-Programming-Fundamentals/09-Text-Processing/Lecture/src/_01_ReverseStrings.java
+++ Java-Programming-Fundamentals/09-Text-Processing/Lecture/src/_01_ReverseStrings.java
@@ -1,6 +1,6 @@
 import java.util.Scanner;
 
-public class _01_RepeatStrings {
+public class _01_ReverseStrings {
 
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
