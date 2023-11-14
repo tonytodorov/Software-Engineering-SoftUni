984b5a6ea70712a04ed43647010c5c5017e19d4a
tonytodorov <tonytodorov@protonmail.com>
2023-11-14 20:05:00 +0200

    Create _02_SoftUniBarIncome.java

---
+++ Java-Programming-Fundamentals/10-Regular-Expressions/Exercise/src/_02_SoftUniBarIncome.java
@@ -0,0 +1,49 @@
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Scanner;
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
+
+public class _02_SoftUniBarIncome {
+
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+
+        String command = scanner.nextLine();
+        List<String> matches = new ArrayList<>();
+
+        Pattern pattern = Pattern.compile("(?<customer>\\%[A-Z]?[a-z]+\\%)(?<product>\\<\\w+\\>)([\\W\\w]*)" +
+                "(?<count>\\|[\\d]\\|)\\3(?<price>[\\d.]+\\$)");
+
+        double totalPrice = 0.00;
+
+        while (!command.equals("end of shift")) {
+            matches.add(command);
+            command = scanner.nextLine();
+        }
+
+        for (String text : matches) {
+            Matcher matcher = pattern.matcher(text);
+
+            if (matcher.find()) {
+                String customer = matcher.group("customer");
+                String product = matcher.group("product");
+                String count = matcher.group("count");
+                String price = matcher.group("price");
+
+                customer = customer.replace("%", "");
+                product = product.replace("<", "").replace(">", "");
+                count = count.replace("|", "");
+                price = price.replace("$", "");
+
+                int currentCount = Integer.parseInt(count);
+                double currentPrice = Double.parseDouble(price);
+
+                totalPrice += currentCount * currentPrice;
+                System.out.printf("%s: %s - %.2f%n", customer, product, currentCount * currentPrice);
+            }
+        }
+
+        System.out.printf("Total income: %.2f", totalPrice);
+    }
+}
