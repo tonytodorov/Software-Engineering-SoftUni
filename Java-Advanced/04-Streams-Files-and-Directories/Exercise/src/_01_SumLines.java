fef592ca020c18c1b97bd17e8d01c3790a38f3f6
tonytodorov <tonytodorov@protonmail.com>
2024-01-05 22:01:52 +0200

    Add Streams-Files-and-Directories Exercise

---
+++ Java-Advanced/04-Streams-Files-and-Directories/Exercise/src/_01_SumLines.java
@@ -0,0 +1,23 @@
+import java.io.*;
+
+public class _01_SumLines {
+
+    public static void main(String[] args) throws IOException {
+
+        String path = "C:\\Users\\PC-TONY\\Desktop\\04. Java-Advanced-Files-and-Streams-Exercises-Resources\\input.txt";
+        BufferedReader bufferedReader = new BufferedReader(new InputStreamReader(new FileInputStream(path)));
+
+        String line = bufferedReader.readLine();
+
+        while (line != null) {
+            int sum = 0;
+
+            for (char currentChar: line.toCharArray()) {
+                sum += currentChar;
+            }
+
+            System.out.println(sum);
+            line = bufferedReader.readLine();
+        }
+    }
+}
