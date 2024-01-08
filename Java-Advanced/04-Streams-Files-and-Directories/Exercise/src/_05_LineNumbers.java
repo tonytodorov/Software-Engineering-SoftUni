f8e2818414dc980dbdf9d9473f092e53065d882d
Tony Todorov <tonytodorov@protonmail.com>
2024-01-08 17:24:43 +0200

    Create _05_LineNumbers.java

---
+++ Java-Advanced/04-Streams-Files-and-Directories/Exercise/src/_05_LineNumbers.java
@@ -0,0 +1,26 @@
+import java.io.*;
+
+public class _05_LineNumbers {
+
+    public static void main(String[] args) throws IOException {
+
+        String path = "C:\\Users\\PC-TONY\\Desktop\\" +
+                "04. Java-Advanced-Files-and-Streams-Exercises-Resources\\inputLineNumbers.txt";
+        BufferedReader bufferedReader = new BufferedReader
+                (new InputStreamReader(new FileInputStream(path)));
+
+        FileWriter fileWriter = new FileWriter("line-numbers");
+
+        String line = bufferedReader.readLine();
+        int count = 1;
+
+        while (line != null) {
+            fileWriter.write(String.format("%d. %s%n", count, line));
+            count += 1;
+
+            line = bufferedReader.readLine();
+        }
+
+        fileWriter.flush();
+    }
+}
