7e70b20b14cc9b6d1cb946b4fac229362916f8b1
Tony Todorov <tonytodorov@protonmail.com>
2024-01-09 14:29:45 +0200

    Create _03_AllCapitals.java

---
+++ Java-Advanced/04-Streams-Files-and-Directories/Exercise/src/_03_AllCapitals.java
@@ -0,0 +1,21 @@
+import java.io.*;
+import java.util.Locale;
+
+public class _03_AllCapitals {
+
+    public static void main(String[] args) throws IOException {
+
+        String path = "C:\\Users\\PC-TONY\\Desktop\\04. Java-Advanced-Files-and-Streams-Exercises-Resources\\input.txt";
+        BufferedReader bufferedReader = new BufferedReader(new InputStreamReader(new FileInputStream(path)));
+
+        String line = bufferedReader.readLine();
+        FileWriter fileWriter = new FileWriter("all-capitals");
+
+        while (line != null) {
+            fileWriter.write(String.format("%s%n", line.toUpperCase(Locale.ROOT)));
+            line = bufferedReader.readLine();
+        }
+
+        fileWriter.flush();
+    }
+}
