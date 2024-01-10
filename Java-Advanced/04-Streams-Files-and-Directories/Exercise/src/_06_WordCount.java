3fa0649d8560e634429834cb7d50e4b4edf544ed
Tony Todorov <tonytodorov@protonmail.com>
2024-01-10 18:11:17 +0200

    Create _06_WordCount.java

---
+++ Java-Advanced/04-Streams-Files-and-Directories/Exercise/src/_06_WordCount.java
@@ -0,0 +1,32 @@
+import java.io.*;
+import java.util.Scanner;
+
+public class _06_WordCount {
+
+    public static void main(String[] args) throws IOException {
+
+        String wordsPath = "C:\\Users\\PC-TONY\\Desktop\\" +
+                "04. Java-Advanced-Files-and-Streams-Exercises-Resources\\words.txt";
+        String filePath = "C:\\Users\\PC-TONY\\Desktop\\" +
+                "04. Java-Advanced-Files-and-Streams-Exercises-Resources\\text.txt";
+
+        Scanner readWords = new Scanner(new FileInputStream(wordsPath));
+
+        while (readWords.hasNext()) {
+            String currentWord = readWords.next();
+            int count = 0;
+
+            Scanner readFileWords = new Scanner(new FileInputStream(filePath));
+
+            while (readFileWords.hasNext()) {
+                String currentFileWord = readFileWords.next();
+
+                if (currentWord.equals(currentFileWord)) {
+                    count += 1;
+                }
+            }
+            
+            System.out.printf("%s - %d%n", currentWord, count);
+        }
+    }
+}
