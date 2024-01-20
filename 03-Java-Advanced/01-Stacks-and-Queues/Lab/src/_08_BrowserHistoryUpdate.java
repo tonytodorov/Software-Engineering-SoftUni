3f04cdbb4408ba3113996d88a54186e3271b1e66
Tony Todorov <tonytodorov@protonmail.com>
2024-01-20 12:17:18 +0200

    Update _08_BrowserHistoryUpdate.java

--- 03-Java-Advanced/01-Stacks-and-Queues/Lab/src/_08_BrowserHistoryUpdate.java
+++ 03-Java-Advanced/01-Stacks-and-Queues/Lab/src/_08_BrowserHistoryUpdate.java
@@ -14,37 +14,26 @@ public class _08_BrowserHistoryUpdate {
         while (!command.equals("Home")) {
 
             if (command.equals("back")) {
-                back(command, stack);
+               if (!stack.isEmpty()) {
+                   stack.pop();
+                   System.out.println(stack.peek());
+               } else {
+                   System.out.println("no previous URLs");
+               }
+            } else if (command.equals("forward")) {
+                if (!queue.isEmpty()) {
+                    queue.poll();
+                    System.out.println(queue.peek());
+                } else {
+                    System.out.println("no next URLs");
+                }
             } else {
-                forward(command, queue);
+                stack.push(command);
+                queue.offer(command);
+                System.out.println(command);
             }
 
             command = scanner.nextLine();
-        }
-    }
-
-    public static void back(String command, ArrayDeque<String> stack) {
-
-        if (!stack.isEmpty() && stack.size() != 1) {
-            stack.pop();
-        } else if (stack.isEmpty()){
-            System.out.println("no previous URLs");
-        } else{
-            stack.push(command);
-            System.out.println(stack.peek());
-        }
-
-    }
-
-    public static void forward(String command, ArrayDeque<String> queue) {
-
-        if (!queue.isEmpty() && queue.size() != 1) {
-            queue.clear();
-        } else if (queue.isEmpty()){
-            System.out.println("no previous URLs");
-        } else{
-            queue.push(command);
-            System.out.println(queue.peek());
         }
     }
 }
