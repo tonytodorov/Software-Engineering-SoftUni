e533a1e292f427efdbece65858251c41fbae9296
Tony Todorov <tonytodorov@protonmail.com>
2024-02-07 20:20:57 +0200

    Update Main.java

--- 03-Java-Advanced/06-Defining-Classes/Exercise/src/company/roster/Main.java
+++ 03-Java-Advanced/06-Defining-Classes/Exercise/src/company/roster/Main.java
@@ -4,7 +4,7 @@ import java.util.*;
 
 import static java.util.stream.Collectors.groupingBy;
 
-public class MainEmployee {
+public class Main {
 
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
