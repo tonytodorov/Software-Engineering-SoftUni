fa0e9ed305aabdcb75e31841b9eb4530961e05fd
Tony Todorov <tonytodorov@protonmail.com>
2024-02-22 18:51:55 +0200

    Add Working-with-Abstraction Exercise

---
+++ 04-Java-OOP/01-Working-with-Abstraction/Exercise/src/card_rank/CardRank.java
@@ -0,0 +1,28 @@
+package card_rank;
+
+public enum CardRank {
+
+    ACE(0),
+    TWO(1),
+    THREE(2),
+    FOUR(3),
+    FIVE(4),
+    SIX(5),
+    SEVEN(6),
+    EIGHT(7),
+    NINE(8),
+    TEN(9),
+    JACK(10),
+    QUEEN(11),
+    KING(12);
+
+    private final int value;
+
+    CardRank(int value) {
+        this.value = value;
+    }
+
+    public int getValue() {
+        return value;
+    }
+}
