d9aec4e8ee110a6bc97d17de4ecb9d70f8c7b73f
Tony Todorov <tonytodorov@protonmail.com>
2024-02-27 17:58:24 +0200

    Add tasks Inheritance Lab

---
+++ 04-Java-OOP/03-Inheritance/Lab/src/single_inheritance/Main.java
@@ -0,0 +1,10 @@
+package single_inheritance;
+
+public class Main {
+
+    public static void main(String[] args) {
+        Dog dog = new Dog();
+        dog.eat();
+        dog.bark();
+    }
+}
---
+++ 04-Java-OOP/03-Inheritance/Lab/src/stack_of_strings/Main.java
@@ -0,0 +1,8 @@
+package stack_of_strings;
+
+public class Main {
+
+    public static void main(String[] args) {
+
+    }
+}
