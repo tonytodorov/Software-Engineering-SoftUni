8a7d9d2948e7f04995cb16476c20b667115df91f
Tony Todorov <tonytodorov@protonmail.com>
2024-02-29 18:35:34 +0200

    Add task Inheritance-Exercise

---
+++ 04-Java-OOP/03-Inheritance/Exercise/src/vehicle/Main.java
@@ -0,0 +1,8 @@
+package vehicle;
+
+public class Main {
+
+    public static void main(String[] args) {
+
+    }
+}
---
+++ 04-Java-OOP/03-Inheritance/Exercise/src/vehicle/RaceMotorcycle.java
@@ -0,0 +1,9 @@
+package vehicle;
+
+public class RaceMotorcycle extends Motorcycle{
+
+    public RaceMotorcycle(double fuel, int horsePower) {
+        super(fuel, horsePower);
+        super.setFuelConsumption(8);
+    }
+}
