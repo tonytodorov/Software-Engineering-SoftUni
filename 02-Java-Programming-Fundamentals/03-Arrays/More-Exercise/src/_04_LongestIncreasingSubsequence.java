d8841d7ba0d90c36ac2a6572cb8fcc0aa41c48ca
Tony Todorov <tonytodorov@protonmail.com>
2024-03-01 20:00:04 +0200

    Add task Inheritance Exercise

--- 02-Java-Programming-Fundamentals/03-Arrays/More-Exercise/src/_04_LongestIncreasingSubsequence.java
+++ 02-Java-Programming-Fundamentals/03-Arrays/More-Exercise/src/_04_LongestIncreasingSubsequence.java
@@ -1,1 +1,10 @@
+import java.util.Scanner;
 
+public class _04_LongestIncreasingSubsequence {
+
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+
+
+    }
+}
