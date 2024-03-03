9482511f532a4a49d6d90d87764f78c94a87092f
Tony Todorov <tonytodorov@protonmail.com>
2024-03-03 20:36:46 +0200

    Add Workshop-Lab

--- 03-Java-Advanced/07-Workshop/Lab/src/SmartArray.java
+++ 03-Java-Advanced/07-Workshop/Lab/src/SmartArray.java
@@ -1,3 +1,5 @@
+import java.util.function.Consumer;
+
 public class SmartArray {
 
     private int[] data;
@@ -26,7 +28,56 @@ public class SmartArray {
         this.data = copy;
     }
 
-    public void get(int index) {
-        
+    public int get(int index) {
+        if (!isValidIndex(index)) {
+            throw new IndexOutOfBoundsException();
+        }
+
+        return this.data[index];
+    }
+
+    public int remove(int index) {
+        if (!isValidIndex(index)) {
+            throw new IndexOutOfBoundsException();
+        }
+
+        int[] copyArr = new int[data.length];
+        int currentIndex = 0;
+
+        for (int i = 0; i < data.length; i++) {
+            if (data[i] != data[index]) {
+                copyArr[currentIndex] = data[i];
+            } else {
+                currentIndex -= 1;
+            }
+
+            currentIndex++;
+        }
+
+        return -1;
+    }
+
+    public boolean contains(int element) {
+        for (int currentNum : data) {
+            if (currentNum == element) {
+                return true;
+            }
+        }
+
+        return false;
+    }
+
+    public void add(int firstIndex, int secondIndex) {
+
+    }
+
+    public void forEach(Consumer<Integer> consumer) {
+        for (int element : this.data) {
+            consumer.accept(element);
+        }
+    }
+
+    private boolean isValidIndex(int index) {
+        return index >= 0 && index <= this.data.length;
     }
 }
