41477e53cf3f5622e2c119ba129e5c817e46ecb0
Tony Todorov <tonytodorov@protonmail.com>
2024-03-04 23:03:31 +0200

    Refactor Workshop Lab

--- 03-Java-Advanced/07-Workshop/Lab/src/Main.java
+++ 03-Java-Advanced/07-Workshop/Lab/src/Main.java
@@ -1,11 +1,6 @@
-import java.util.Arrays;
-import java.util.Scanner;
-
 public class Main {
 
     public static void main(String[] args) {
-        Scanner scanner = new Scanner(System.in);
-
 
     }
 }
--- 03-Java-Advanced/07-Workshop/Lab/src/SmartArray.java
+++ 03-Java-Advanced/07-Workshop/Lab/src/SmartArray.java
@@ -22,11 +22,18 @@ public class SmartArray {
     }
 
     public void resize() {
-        this.capacity *= 2;
+        this.capacity = this.capacity * 2;
+        copyArr();
+    }
+
+    public void shrink() {
+        this.capacity = this.capacity / 2;
+        copyArr();
+    }
+
+    private void copyArr() {
         int[] copy = new int[this.capacity];
-
         System.arraycopy(this.data, 0, copy, 0, this.data.length);
-
         this.data = copy;
     }
 
@@ -69,7 +76,43 @@ public class SmartArray {
     }
 
     public void add(int firstIndex, int secondIndex) {
-        
+
+    }
+
+    public void shiftLeft(int index) {
+
+        int[] copyArr = new int[this.data.length - 1];
+        int startIndex = 0;
+
+        for (int i = 0; i < this.data.length; i++) {
+            if (index != i) {
+                copyArr[startIndex] = this.data[i];
+                startIndex++;
+            }
+        }
+
+        this.data = copyArr;
+    }
+
+    public void shiftRight(int index) {
+        int[] copyLeftPart = new int[index];
+        int[] copyRightPart = new int[data.length - index];
+
+        for (int i = 0; i < copyLeftPart.length; i++) {
+            copyLeftPart[i] = data[i];
+        }
+
+        for (int i = 0; i < copyRightPart.length; i++) {
+            copyRightPart[i] = data[index + i];
+        }
+
+        int[] result = new int[data.length + 1];
+
+
+        System.arraycopy(copyLeftPart, 0, result, 0, result.length);
+        System.arraycopy(copyRightPart, 0, result, 0, result.length);
+
+        this.data = result;
     }
 
     public void forEach(Consumer<Integer> consumer) {
