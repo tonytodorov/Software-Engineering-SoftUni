fbc886f6416d5bade8d6f94dfe2ee871470df4d2
Tony Todorov <tonytodorov@protonmail.com>
2024-03-11 21:27:21 +0200

    Add task Interfaces-and-Abstraction Lab

---
+++ 04-Java-OOP/04-Interfaces-and-Abstraction/Lab/src/say_hello/Main.java
@@ -0,0 +1,8 @@
+package say_hello;
+
+public class Main {
+
+    public static void main(String[] args) {
+
+    }
+}
---
+++ 04-Java-OOP/04-Interfaces-and-Abstraction/Lab/src/say_hello/Person.java
@@ -0,0 +1,7 @@
+package say_hello;
+
+public interface Person {
+
+    String getName();
+    String sayHello();
+}
