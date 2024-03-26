5e8d3bc7671f499d1b9d972941e5533096ea32f0
Tony Todorov <tonytodorov@protonmail.com>
2024-03-26 15:00:58 +0200

    Refactor class

--- 04-Java-OOP/12-Exams/Exam-2023-December-19/ClimbersApp/src/main/java/climbers/models/climbing/ClimbingImpl.java
+++ 04-Java-OOP/12-Exams/Exam-2023-December-19/ClimbersApp/src/main/java/climbers/models/climbing/ClimbingImpl.java
@@ -6,7 +6,7 @@ import climbers.models.mountain.Mountain;
 import java.util.Collection;
 
 public class ClimbingImpl implements Climbing {
-    
+
     @Override
     public void conqueringPeaks(Mountain mountain, Collection<Climber> climbers) {
 
