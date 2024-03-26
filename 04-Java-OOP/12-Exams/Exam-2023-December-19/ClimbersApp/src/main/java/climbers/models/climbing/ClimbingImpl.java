c6c55aa1a2873a1c6a87d8aec6ce307a11bb5ce6
Tony Todorov <tonytodorov@protonmail.com>
2024-03-26 14:58:15 +0200

    Modify ClimbingImpl

--- 04-Java-OOP/12-Exams/Exam-2023-December-19/ClimbersApp/src/main/java/climbers/models/climbing/ClimbingImpl.java
+++ 04-Java-OOP/12-Exams/Exam-2023-December-19/ClimbersApp/src/main/java/climbers/models/climbing/ClimbingImpl.java
@@ -1,14 +1,12 @@
 package climbers.models.climbing;
 
-import climbers.models.climber.BaseClimber;
 import climbers.models.climber.Climber;
 import climbers.models.mountain.Mountain;
 
 import java.util.Collection;
 
 public class ClimbingImpl implements Climbing {
-
-    private BaseClimber baseClimber;
+    
     @Override
     public void conqueringPeaks(Mountain mountain, Collection<Climber> climbers) {
 
