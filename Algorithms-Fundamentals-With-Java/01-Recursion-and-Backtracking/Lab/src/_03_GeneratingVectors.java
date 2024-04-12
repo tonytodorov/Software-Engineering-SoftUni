35efa629e44cf4d6d42698ad9433bed024aef63a
Tony Todorov <tonytodorov@protonmail.com>
2024-04-12 20:17:02 +0300

    Add task Recursion-and-Backtracking

---
+++ Algorithms-Fundamentals-With-Java/01-Recursion-and-Backtracking/Lab/src/_03_GeneratingVectors.java
@@ -0,0 +1,36 @@
+import java.util.Scanner;
+
+public class _03_GeneratingVectors {
+
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+
+        int n = Integer.parseInt(scanner.nextLine());
+
+        Integer[] memory = new Integer[n];
+
+        fillVector(memory, 0);
+
+    }
+
+    private static void fillVector(Integer[] memory, int index) {
+        if (index == memory.length) {
+            print(memory);
+            return;
+        }
+
+        for (int i = 0; i <= 1; i++) {
+            memory[index] = i;
+            fillVector(memory, index + 1);
+        }
+
+    }
+
+    private static void print(Integer[] memory) {
+        for (Integer value : memory) {
+            System.out.print(value);
+        }
+
+        System.out.println();
+    }
+}
