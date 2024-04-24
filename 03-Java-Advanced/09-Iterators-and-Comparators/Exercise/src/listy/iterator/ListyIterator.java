36669d100230230607a006547963fe89df71f778
Tony Todorov <tonytodorov@protonmail.com>
2024-04-24 18:20:27 +0300

    Refactor classes

--- 03-Java-Advanced/09-Iterators-and-Comparators/Exercise/src/listy/iterator/ListyIterator.java
+++ 03-Java-Advanced/09-Iterators-and-Comparators/Exercise/src/listy/iterator/ListyIterator.java
@@ -4,29 +4,9 @@ import java.util.Iterator;
 
 public class ListyIterator implements Iterable<String>{
 
-    private String[] stringList;
-
-    public ListyIterator(String... stringList) {
-        this.stringList = stringList;
-    }
 
     @Override
     public Iterator<String> iterator() {
-        return new ListIterator();
+        return null;
     }
-
-    class ListIterator implements Iterator<String> {
-
-        private int index = 0;
-        @Override
-        public boolean hasNext() {
-            return index < stringList.length;
-        }
-
-        @Override
-        public String next() {
-            return stringList[index++];
-        }
-    }
-
 }
