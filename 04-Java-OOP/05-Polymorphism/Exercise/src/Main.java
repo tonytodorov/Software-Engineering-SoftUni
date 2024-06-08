2a225a1f0d16219408af9cafc40da02011e95354
Tony Todorov <tonytodorov@protonmail.com>
2024-06-08 19:38:52 +0300

    Update Main.java

--- 04-Java-OOP/05-Polymorphism/Exercise/src/Main.java
+++ 04-Java-OOP/05-Polymorphism/Exercise/src/Main.java
@@ -1,6 +1,12 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+
 public class Main {
 
-    public static void main(String[] args) {
+    public static void main(String[] args) throws IOException {
 
+        BufferedReader bf = new BufferedReader(new InputStreamReader(System.in));
+        bf.read();
     }
 }
