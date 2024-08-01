2ae7c3ded4b704c0be527aaf5da96fc85e711420
Tony Todorov <tonytodorov@protonmail.com>
2024-08-01 09:59:18 +0300

    Refactor task

--- 05-JS-Front-End/02-JS-Front-End/05-DOM-Introduction/Exercises/08. Generate Report/report.js
+++ 05-JS-Front-End/02-JS-Front-End/05-DOM-Introduction/Exercises/08. Generate Report/report.js
@@ -1,15 +1,34 @@
 function generateReport() {
+
+    class ReportData {
+    
+    }
+    
     const checkBoxes = document.querySelectorAll('table thead tr th input[type=checkbox]')
     const trElements = document.querySelectorAll('table tbody tr')
-    console.log(checkBoxes);
-    console.log(trElements);
+    const resultElement = document.getElementById('output')
 
+    let result = []
+    
+    for (let i = 0; i < trElements.length; i++) {
+        const data = new ReportData()
+        result.push(data)
+    }
+    
     for (let i = 0; i < checkBoxes.length; i++) {
         const element = checkBoxes[i];
-
-        if (element.checked === true) {
-
-        }
         
+        if (element.checked === true) {
+            const field = document.querySelector('table thead tr')
+            const propertyName = field.children[i].textContent.trim().toLocaleLowerCase()
+
+            for (let j = 0; j < trElements.length; j++) {
+                const element = trElements[j].children[i].innerText
+
+                result[j][propertyName] = element
+            }
+        }
     }
+
+    resultElement.textContent = JSON.stringify(result, null, ' ')
 }
--- 05-JS-Front-End/02-JS-Front-End/05-DOM-Introduction/Exercises/09. Number Convertor/solution.js
+++ 05-JS-Front-End/02-JS-Front-End/05-DOM-Introduction/Exercises/09. Number Convertor/solution.js
@@ -1,4 +1,33 @@
 function solve() {
+    const button = document.querySelector('button')
+    const resultElement = document.querySelector('#result')
+    
+    let toMenu = document.querySelector('#selectMenuTo')
+    let input = document.querySelector('#input')
+    
+    const optionsMenu = {
+        'Binary': 'binary',
+        'Hexadecimal': 'hexadecimal',
+    }
+    
+    for (const key in optionsMenu) {
+        let createOption = document.createElement('option')
+        createOption.value = optionsMenu[key]
+        createOption.textContent = key
+        toMenu.appendChild(createOption)
+    }
+    
+    function convert() {
+        debugger
+        const [number, optionResult] = [Number(input.value), toMenu.value]
 
-    //TODO...
+        if (optionResult === 'binary') {
+            resultElement.value = number.toString(2)
+        } else if (optionResult === 'hexadecimal') {
+            resultElement.value = number.toString(16).toUpperCase() 
+        }
+        
+    }
+    
+    button.addEventListener('click', convert)
 }
