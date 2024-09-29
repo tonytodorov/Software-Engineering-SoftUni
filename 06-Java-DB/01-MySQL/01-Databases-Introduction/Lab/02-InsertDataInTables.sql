bc0152c80d5ab6c7fabe1531cba6e143abd8bc3b
Tony Todorov <tonytodorov@protonmail.com>
2024-09-29 10:18:58 +0300

    Add queries

---
+++ 06-Java-DB/01-MySQL/01-Databases-Introduction/Lab/02-InsertDataInTables.sql
@@ -0,0 +1,7 @@
+# Insert values into table
+INSERT INTO `gamebar`.`employees` (first_name, last_name)
+VALUES ('Test1', 'Test1'),
+	   ('Test2', 'Test2'),
+       ('Test3', 'Test3');
+       
+SELECT * FROM employees;
---
+++ 06-Java-DB/01-MySQL/01-Databases-Introduction/Lab/03-AlterTables.sql
@@ -0,0 +1,5 @@
+# Add column to employees table
+ALTER TABLE employees
+ADD middle_name VARCHAR(50);
+
+SELECT * FROM employees;
