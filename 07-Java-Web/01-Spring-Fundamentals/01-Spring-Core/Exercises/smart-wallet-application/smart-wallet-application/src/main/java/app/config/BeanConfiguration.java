8f74b6127bb8acadce569aba5310106cb167a5b4
Tony Todorov <tonytodorov@protonmail.com>
2025-02-18 14:22:35 +0200

    Add exam-preparation

--- 07-Java-Web/01-Spring-Fundamentals/01-Spring-Core/Exercises/smart-wallet-application/smart-wallet-application/src/main/java/app/config/BeanConfiguration.java
+++ 07-Java-Web/01-Spring-Fundamentals/01-Spring-Core/Exercises/smart-wallet-application/smart-wallet-application/src/main/java/app/config/BeanConfiguration.java
@@ -3,6 +3,7 @@ package app.config;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.security.crypto.password.NoOpPasswordEncoder;
 import org.springframework.security.crypto.password.PasswordEncoder;
 
 @Configuration
--- 07-Java-Web/01-Spring-Fundamentals/01-Spring-Core/Exercises/smart-wallet-application/smart-wallet-application/src/main/java/app/config/WebMvcConfiguration.java
+++ 07-Java-Web/01-Spring-Fundamentals/01-Spring-Core/Exercises/smart-wallet-application/smart-wallet-application/src/main/java/app/config/WebMvcConfiguration.java
@@ -2,7 +2,11 @@ package app.config;
 
 import app.security.SessionCheckInterceptor;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.autoconfigure.security.servlet.PathRequest;
+import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.web.SecurityFilterChain;
 import org.springframework.web.servlet.config.annotation.InterceptorRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
@@ -17,5 +21,27 @@ public class WebMvcConfiguration implements WebMvcConfigurer {
         registry.addInterceptor(sessionCheckInterceptor)
                 .addPathPatterns("/**")
                 .excludePathPatterns("/css/**", "/images/**");
+    }
+
+    @Bean
+    public SecurityFilterChain securityFilterChain(HttpSecurity httpSecurity) throws Exception {
+
+        httpSecurity
+                .authorizeRequests(request -> request
+                .requestMatchers(PathRequest.toStaticResources().atCommonLocations()).permitAll()
+                .requestMatchers("/", "/register").permitAll()
+//                .requestMatchers("/users").hasRole("ADMIN")
+                .anyRequest().authenticated()
+        )
+                .formLogin(form -> form
+                        .loginPage("/login")
+                        .defaultSuccessUrl("/home")
+                        .failureUrl("/login?error")
+                        .permitAll()
+                );
+
+
+
+        return httpSecurity.build();
     }
 }
