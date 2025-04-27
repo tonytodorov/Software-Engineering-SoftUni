c9fcc750450a5ca7af7197d995ff77db2acd1259
Tony Todorov <tonytodorov@protonmail.com>
2025-04-27 18:26:58 +0300

    Add task

---
+++ 07-Java-Web/02-Spring-Advanced/01-REST-API-And-REST-With-Spring/Lab/products-rest-sevice-demo/src/test/java/app/productsrestsevicedemo/ProductsRestSeviceDemoApplicationTests.java
@@ -0,0 +1,13 @@
+package app.productsrestsevicedemo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ProductsRestSeviceDemoApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
